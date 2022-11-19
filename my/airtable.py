19c69b22839dbbb4c5298b0107b0aeac7daa65b2
Madeline Cameron <maddie@qnzl.co>
2022-11-19 17:43:58 -0500

    Add airtable

---
+++ my/airtable.py
@@ -0,0 +1,38 @@
+'''
+[[https://airtable.com][Airtable]] data
+'''
+
+REQUIRES = [
+    'git+https://github.com/hpi/airtable',
+]
+
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Sequence, Iterable
+
+from my.core import Paths, get_files
+
+from my.config import airtable as user_config
+
+@dataclass
+class airtable(user_config):
+    # paths[s]/glob to the exported JSON data
+    export_path: Paths
+
+
+def inputs() -> Sequence[Path]:
+    return get_files(airtable.export_path)
+
+
+import airtable.dal as dal
+
+def sessions():
+    _dal = dal.DAL(inputs())
+    yield from _dal.sessions()
+
+
+from my.core.pandas import check_dataframe, DataFrameT
+
+def records(baseName = None, tableName = None):
+    _dal = dal.DAL(inputs())
+    yield from _dal.records(baseName, tableName)
